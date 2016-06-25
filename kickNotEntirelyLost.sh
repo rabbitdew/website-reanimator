cd76f37125d6369c89124156406d7aa476a2606e
rabbitdew <rabbitdew@users.noreply.github.com>
2016-06-25 12:56:26 -0700

    Add files via upload

---
+++ kickNotEntirelyLost.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#rm -rf /etc/yum.repos.d/*
+
+#mount -o loop /dev/sr0 /mnt/
+#echo -e "[DVDrepo]\nbaseurl=file:///mnt/\nenabled=1\ngpgcheck=0\n" > /etc/yum.repos.d/dvd.repo
+#read -p "Enter path to tar backup" $WWw_BACKUP
+if [ -f "$1" ] 
+
+then
+
+read -p "Enter a root mariadb password:"  DATABASE_PASS
+read -p "Enter a name for the wp database:" DATABASE_WP
+read -p "Enter a username to use that database:" DATABASE_USER
+read -p "Enter a password for that username:" DATABASE_WP_PW
+
+yum -y clean all
+yum -y upgrade
+yum -y install firewalld rsync php-gd php php-mysql policycoreutils mariadb mariadb-server httpd wget
+
+systemctl enable firewalld
+systemctl start firewalld
+systemctl enable httpd
+systemctl start httpd
+
+firewall-cmd --add-service=http --permanent
+firewall-cmd --add-service=http
+
+
+mv "$1" /tmp
+
+cd /tmp
+wget http://wordpress.org/latest.tar.gz
+tar -xvJf "$1"
+
+rsync -avP /tmp/var/www/html/ /var/www/html --exclude=lifeBlog
+
+mkdir -p /var/www/html/lifeBlog
+
+tar -xvzf latest.tar.gz
+
+rsync -avP /tmp/wordpress/ /var/www/html/lifeBlog
+
+cd /var/www/html/lifeBlog/
+mv -f wp-config-sample.php wp-config.php
+
+systemctl enable mariadb
+systemctl start mariadb
+
+
+mysqladmin -u root password "$DATABASE_PASS"
+mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+mysql -u root -p"$DATABASE_PASS" -e "CREATE DATABASE $DATABASE_WP" 
+mysql -u root -p"$DATABASE_PASS" -e "GRANT ALL PRIVILEGES ON $DATABASE_WP.* TO '$DATABASE_USER'@'localhost' IDENTIFIED BY '$DATABASE_WP_PW'"
+mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+sed -i 's/database_name_here/'$DATABASE_WP'/' /var/www/html/lifeBlog/wp-config.php
+sed -i 's/username_here/'$DATABASE_USER'/' /var/www/html/lifeBlog/wp-config.php
+sed -i 's/password_here/'$DATABASE_WP_PW'/' /var/www/html/lifeBlog/wp-config.php
+  
+rsync -avP /tmp/var/www/html/lifeBlog/wp-content/ /var/www/html/lifeBlog/wp-content/
+chown -R apache:apache /var/www/html/
+
+chmod 600 /var/www/html/lifeBlog/wp-config.php
+
+#finish installation in web browser, then restore database: 
+#mysql -u root -p < /tmp/wpDB.sql 
+
+#rm -rf /tmp/latest.tar.gz /tmp/wordpress /tmp/var/"
+
+echo -e "ServerTokens Prod\nServerSignature Off\nTraceEnable Off\n" >> /etc/httpd/httpd.conf
+exit 0 
+
+else 
+ echo "error: add backup tar as parameter"
+ exit 1 
+fi
+
